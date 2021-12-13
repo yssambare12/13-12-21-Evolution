76a3b488b656ed8d0809cec2eacac3607ed1c0d1
Yogesh  Sambare <bloggingscan@gmail.com>
2021-12-13 16:47:17 +0530

    Update db1.js

--- src/config/db1.js
+++ src/config/db1.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
 
 module.exports =()=>{
-    return mongoose.connect(" mongodb://127.0.0.1:27017/C413Dec");
+    return mongoose.connect(" mongodb://127.0.0.1:27017/C4Dec");
 }
